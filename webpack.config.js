825207546ad19ddd47ee3816ab4cde1967f8f51c
Matt Zaso <matt.zaso@odopod.com>
2017-10-27 15:23:39 -0700

    Initial Commit. Creating directory structure and adding in webpack to package up all JS (and eventually css)

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+var DIST_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: SRC_DIR + '/index.js',
+  output: {
+    path: DIST_DIR + '/',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react'],
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
