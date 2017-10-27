e1096af1325e5c164446d31e930074e44a7e8e1f
Matt Zaso <matt.zaso@odopod.com>
2017-10-27 16:26:54 -0700

    Adding in grid scss for style and layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,12 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6"><p>Testing grid</p></div>
+            <div className="col-sm-6"><p>Testing grid</p></div>
+          </div>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ var config = {
   },
   plugins: [
     extractPlugin
-  ]
+  ],
 };
 
 module.exports = config;
