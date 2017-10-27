58a346f41bed9d0e148760f9c9bb74b9d6ced28e
Matt Zaso <matt.zaso@odopod.com>
2017-10-27 16:54:49 -0700

    Creating API key on giphy and adding it for future reference

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './css/main.scss'
 
+const API_KEY = '5u1dZor3NNYiALYROwvO7wSEpa05Q3Al';
+const TRENDING_URL = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&rating=G`;
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
