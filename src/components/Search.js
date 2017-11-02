0d51e175c8616021006ad286c4c6cf1c48c43617
Matt Zaso <matt.zaso@odopod.com>
2017-11-01 22:00:42 -0700

    Making the search a dumb component for simplicity

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <form className="search-form col-12" onSubmit={this.props.on_search}>
-        <input className="search-form__input" type="text" name="search" />
-        <button className="search-form__button">Search</button>
-      </form>
-    );
-  }
+export const Search = (props) => {
+  return (
+    <form className="search-form col-12" onSubmit={this.props.on_search}>
+      <input className="search-form__input" type="text" name="search" />
+      <button className="search-form__button">Search</button>
+    </form>
+  );
 }
-
-export default Search;
